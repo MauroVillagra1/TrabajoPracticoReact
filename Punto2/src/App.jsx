1e733243bbcc1bee7a78d1c9126f6c5c9ec79415
Mauro Villagra <maurovillagra257@gmail.com>
2023-07-17 19:53:26 -0300

    Primer Commit

---
+++ Punto2/src/App.jsx
@@ -0,0 +1,13 @@
+import Myfriends from './components/Myfriends'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Myfriends text='My Friend'/>
+    </>
+  )
+}
+
+export default App
---
+++ Punto2/src/components/Myfriends.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Myfriends({text="Myfriends"}) {
+  return (
+    <h1>Hello {text}</h1>
+    )
+}
+
+export default Myfriends
